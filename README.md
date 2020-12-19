956cf01560bbfa3ad35ccdcb1d8dc364ae7ea713
Koushal Bhat <bhatkoushal2001@gmail.com>
2020-12-19 13:14:13 +0530

    todo

--- README.md
+++ README.md
@@ -1,2 +1,152 @@
-# TODO_CLI
-Comand Line tool to manange todo list
+
+## Getting started
+
+1. Install CPP: You should have build support for cpp on your system.
+
+2. You are expected to write the code in `todo.cpp` file.
+
+3. You should be able to build the app by running `make app`.
+
+4. Once you are done with the changes you should be able to execute the todo app by running the following command from the terminal.
+
+   **On Windows:**
+
+   ```
+   .\todo.bat
+   ```
+
+   **On \*nix:**
+
+   ```
+   ./todo.sh
+   ```
+## Run Automated Tests
+
+### 1. Install Node.js
+
+You need to have npm installed in your computer for this problem. It comes with Node.js and you can get it by installing Node from https://nodejs.org/en/
+
+### 2. Install dependencies
+
+Run `npm install` to install all dependencies.
+
+### 3. Create Create symbolic link to the executable file
+
+#### On Windows
+
+To create a symbolic link on Windows, you'll need to run either the Windows Command Prompt, or Windows Powershell **with administrator privileges**. To do so, right-click on the icon for Command Prompt, or Powershell, and choose the _"Run as Administrator"_ option.
+
+**Command Prompt:**
+
+```
+> mklink todo todo.bat
+```
+
+**Powershell:**
+
+```
+> cmd /c mklink todo todo.bat
+```
+
+#### On \*nix:
+
+Run the following command in your shell:
+
+```
+$ ln -s todo.sh todo
+```
+
+### 4. Try running tests.
+
+Now run `npm test` and you will see all the tests failing. As you fill in each functionality, you can re-run the tests to see them passing one by one.
+
+## A Note about `/` for Windows Users
+
+In the following sections, you'll see many commands prefixed with `./`, or paths containing the `/` (forward-slash) character.
+
+If you're using the Windows _Command Prompt_, then you'll need to replace `/` with `\` (back-slash) for these commands and paths to work as expected.
+
+On Windows _Powershell_, these substitutions are not required.
+
+
+
+## Usage
+
+### 1. Help
+
+Executing the command without any arguments, or with a single argument `help` prints the CLI usage.
+
+```
+$ ./todo help
+Usage :-
+$ ./todo add "todo item"  # Add a new todo
+$ ./todo ls               # Show remaining todos
+$ ./todo del NUMBER       # Delete a todo
+$ ./todo done NUMBER      # Complete a todo
+$ ./todo help             # Show usage
+$ ./todo report           # Statistics
+```
+
+### 2. List all pending todos
+
+Use the `ls` command to see all the todos that are not yet complete. The most recently added todo should be displayed first.
+
+```
+$ ./todo ls
+[2] change light bulb
+[1] water the plants
+```
+
+### 3. Add a new todo
+
+Use the `add` command. The text of the todo item should be enclosed within double quotes (otherwise only the first word is considered as the todo text, and the remaining words are treated as different arguments).
+
+```
+$ ./todo add "the thing i need to do"
+Added todo: "the thing i need to do"
+```
+
+### 4. Delete a todo item
+
+Use the `del` command to remove a todo item by its number.
+
+```
+$ ./todo del 3
+Deleted todo #3
+```
+
+Attempting to delete a non-existent todo item should display an error message.
+
+```
+$ ./todo del 5
+Error: todo #5 does not exist. Nothing deleted.
+```
+
+### 5. Mark a todo item as completed
+
+Use the `done` command to mark a todo item as completed by its number.
+
+```
+$ ./todo done 1
+Marked todo #1 as done.
+```
+
+Attempting to mark a non-existed todo item as completed will display an error message.
+
+```
+$ ./todo done 5
+Error: todo #5 does not exist.
+```
+
+### 6. Generate a report
+
+Use the `report` command to see the latest tally of pending and completed todos.
+
+```
+$ ./todo report
+yyyy-mm-dd Pending : 1 Completed : 4
+```
+
+## Improving README.md
+
+If you feel like we are missing out steps, feel free to make a pull request. [Github Repo](https://github.com/nseadlc-2020/package-todo-cli-task)
---
+++ todo.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include<fstream>
+#include<string.h>
+#include<string>
+#include<ctime>
+#include<vector>
+
+
+
+void ls()
+{
+    std::vector<std::string> lines;
+    std::fstream file;
+    file.open("todo.txt",std::ios::in);
+    if (file.is_open()){ //checking whether the file is open
+      std::string tp;
+      while(getline(file, tp)){ //read data from file object and put it into string.
+         lines.push_back(tp); //print the data of the string
+      }
+      file.close(); //close the file object.
+   }
+   if(lines.size()==0)
+   {
+       std::cout<<"There are no pending todos!\n";
+       return;
+   }
+   for(int i=lines.size()-1;i>=0;i--)
+   {
+       std::cout<<"["<<i+1<<"]"<<" "<<lines[i]<<"\n";
+   }
+
+}
+
+void add(std::string line)
+{
+    std::fstream file;
+    file.open("todo.txt",std::ios::app);
+    if(file.is_open()) //checking whether the file is open 
+    {
+      file<<line<<'\n'; //inserting text
+      file.close(); //close the file object
+   }
+}
+
+bool del(int num)
+{
+    std::fstream file;
+    file.open("todo.txt",std::ios::in);
+    std::ofstream ofs; 
+    ofs.open("temp.txt", std::ofstream::out); 
+    char c; 
+    int line_no = 1;
+    bool check=false; 
+    while (file.get(c)) 
+    { 
+        // if a newline character 
+        if(line_no==num)check=true;
+  
+        // file content not to be deleted 
+        if (line_no != num) 
+            ofs << c; 
+         if (c == '\n') 
+        line_no++; 
+    } 
+    ofs.close(); 
+  
+    // closing input file 
+    file.close(); 
+  
+    // remove the original file 
+    remove("todo.txt"); 
+  
+    // rename the file 
+    rename("temp.txt","todo.txt" ); 
+    return check;
+}
+
+bool done(int num)
+{
+    time_t now = time(0);
+    tm *gmtm = localtime(&now);
+    std::fstream file;
+    file.open("todo.txt",std::ios::in);
+    std::ofstream ofs; 
+    ofs.open("temp.txt", std::ofstream::out); 
+    std::fstream done;
+    done.open("done.txt",std::ios::app);
+    int n= 1;
+    if (file.is_open()){ //checking whether the file is open
+      std::string tp;
+      while(getline(file, tp)){ //read data from file object and put it into string.
+         if(n==num)
+         {
+             done<<"x "<<1900+gmtm->tm_year<<"-"<<1+gmtm->tm_mon<<"-"<<gmtm->tm_mday<<" "<<tp<<"\n";
+         }
+         
+         else if(n!=num)
+         {
+             ofs<<tp<<"\n";
+         } 
+         n++;
+      }
+      file.close(); //close the file object.
+   } 
+    ofs.close(); 
+  
+    // closing input file 
+    done.close(); 
+  
+    // remove the original file 
+    remove("todo.txt"); 
+  
+    // rename the file 
+    rename("temp.txt","todo.txt" ); 
+    if(num>n || num<1)
+    return false;
+    else
+    {
+        return true;
+    }
+    
+}
+
+void report()
+{
+    int pending_count=0,done_count=0;
+    time_t now = time(0);
+    tm *gmtm = localtime(&now);
+    std::fstream file;
+    file.open("todo.txt",std::ios::in);
+    std::fstream done;
+    done.open("done.txt",std::ios::in);
+    int n= 1;
+    if (file.is_open()){ //checking whether the file is open
+      std::string tp;
+      while(getline(file, tp)){ //read data from file object and put it into string.
+        pending_count++;
+      }
+      file.close(); //close the file object.
+   } 
+   if (done.is_open()){ //checking whether the file is open
+      std::string tp;
+      while(getline(done, tp)){ //read data from file object and put it into string.
+        done_count++;
+      }
+      done.close(); //close the file object.
+   } 
+   std::cout<<1900+gmtm->tm_year<<"-"<<1+gmtm->tm_mon<<"-"<<gmtm->tm_mday<<" Pending : "<<pending_count<<" Completed : "<<done_count<<"\n";
+
+
+}
+int main(int argc, char* argv[])
+{
+
+    int pendingcount=0;
+    int donecount=0;
+    if(argc==1)
+    {
+        std::cout<<"Usage :-\n$ ./todo add \"todo item\"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics\n";
+    }
+    else if(strcmp(argv[1],"help")==0)
+    {
+        std::cout<<"Usage :-\n$ ./todo add \"todo item\"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics\n";
+    }
+    else if(strcmp(argv[1],"ls")==0)
+    {
+        ls();
+    }
+    else if(strcmp(argv[1],"add")==0)
+    {
+        if(argc==2)
+        {
+            std::cout<<"Error: Missing todo string. Nothing added!\n";
+        }
+        else
+        {
+            add(argv[2]);
+            pendingcount++;
+            std::cout<<"Added todo: \""<<argv[2]<<"\""<<std::endl;
+        }
+    }
+    else if(strcmp(argv[1],"del")==0)
+    {
+        if(argc==2)
+        {
+            std::cout<<"Error: Missing NUMBER for deleting todo.\n";
+        }
+        else
+        {
+            bool check=del(atoi(argv[2]));
+            if(check==true)
+            {
+                pendingcount--;
+                std::cout<<"Deleted todo #"<<argv[2]<<std::endl;
+            }
+            else if(check==false)
+            {
+                std::cout<<"Error: todo #"<<argv[2]<<" does not exist. Nothing deleted."<<std::endl;
+            }
+        }
+        
+
+    }
+    else if(strcmp(argv[1],"done")==0)
+    {
+        if(argc==2)
+        {
+            std::cout<<"Error: Missing NUMBER for marking todo as done.\n";
+        }
+        else
+        {
+            bool check=done(atoi(argv[2]));
+            if(check==true)
+            {
+                pendingcount--;
+                donecount++;
+                std::cout<<"Marked todo #"<<argv[2]<<" as done."<<std::endl;
+            }
+            else if(check==false)
+            {
+                std::cout<<"Error: todo #"<<argv[2]<<" does not exist."<<std::endl;
+            }
+        } 
+
+    }
+    else if(strcmp(argv[1],"report")==0)
+    {
+        report();
+    }
+    
+    return 0;
+}
